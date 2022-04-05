80ca861b3ab64735dbe646a22dbfb1b14cbb3d3a
shaoyuhe <2898549292@qq,com>
2022-04-05 20:51:36 +0800

    vue1

---
+++ day2/单文件组件/App.vue
@@ -0,0 +1,24 @@
+<!--      安装vetur插件后  输入  <v   就可以快熟生成模板-->
+<template>
+<div>
+    <MySchool></MySchool>
+    <MyStudent></MyStudent>
+</div>
+</template>
+
+<script>
+//引入组件
+import MySchool from '../';
+import MyStudent from './Student.Vue';
+export default {
+    name:'App',
+    components:{
+        MySchool,
+        MyStudent
+    }
+}
+</script>
+
+<style>
+
+</style>
